fd40064395ab10243abd8a3c289eea66521215a3
Piyush Singh <singh.piyush00034@gmail.com>
2024-02-20 17:02:02 +0530

    minor changes

--- src/main/java/com/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,6 +1,8 @@
 package com.rest.webservices.restfulwebservices.user;
 
 import jakarta.validation.Valid;
+
+import org.springframework.context.MessageSource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -10,35 +12,37 @@ import java.util.List;
 @RestController
 public class UserResource {
     private UserDAOService service;
-    public UserResource(UserDAOService service){
-        this.service=service;
+    public UserResource(UserDAOService service) {
+        this.service = service;
     }
-//    GET users
+
+    // GET users
     @GetMapping(path = "/users")
-    public List<User> retrieveAllUsers(){
+    public List<User> retrieveAllUsers() {
         return service.findAll();
     }
+
     @GetMapping(path = "/users/{id}")
-    public User retrieveUser(@PathVariable int id){
-        User user=service.findOne(id);
-        if(user==null) throw new UserNotFoundException("id:"+id);
+    public User retrieveUser(@PathVariable int id) {
+        User user = service.findOne(id);
+        if (user == null)
+            throw new UserNotFoundException("id:" + id);
         return user;
     }
+
     @DeleteMapping(path = "/users/{id}")
-    public void deleteUser(@PathVariable int id){
+    public void deleteUser(@PathVariable int id) {
         service.deleteById(id);
     }
 
-    @PostMapping(path="/users")
-    public ResponseEntity<User> createUser(@Valid @RequestBody User user){
-        User savedUser=service.save(user);
-        URI location= ServletUriComponentsBuilder
-                        .fromCurrentRequest()
-                        .path("/{id}")
-                        .buildAndExpand(savedUser.getId())
-                        .toUri();
+    @PostMapping(path = "/users")
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
+        User savedUser = service.save(user);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
         return ResponseEntity.created(location).build();
     }
-
-
 }
