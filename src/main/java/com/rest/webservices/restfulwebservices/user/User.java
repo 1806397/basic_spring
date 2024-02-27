eb681d7a4fb000073d1daabc422c4d8133f282dc
Piyush Singh <singh.piyush00034@gmail.com>
2024-02-27 17:03:22 +0530

    Added serialization

--- src/main/java/com/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/rest/webservices/restfulwebservices/user/User.java
@@ -1,5 +1,6 @@
 package com.rest.webservices.restfulwebservices.user;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 import java.time.LocalDate;
@@ -7,8 +8,10 @@ import java.time.LocalDate;
 public class User {
     private Integer id;
     @Size(min=2,message = "Name should have at least 2 characters")
+    @JsonProperty("user_name")
     private String name;
     @Past(message = "BirthDate should be in past")
+    @JsonProperty("birth_date")
     private LocalDate birthDate;
     @Override
     public String toString() {
