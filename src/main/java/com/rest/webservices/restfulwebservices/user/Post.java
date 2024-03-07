87010c1339ebcfcc9e2ca1283b63afd243d664df
Piyush Singh <singh.piyush00034@gmail.com>
2024-03-07 15:53:02 +0530

    Minor changes

--- src/main/java/com/rest/webservices/restfulwebservices/user/Post.java
+++ src/main/java/com/rest/webservices/restfulwebservices/user/Post.java
@@ -9,20 +9,24 @@ public class Post {
     @Id
     @GeneratedValue
     private Integer id;
-    @Size(min=10)
+    @Size(min = 10)
     private String description;
     @ManyToOne(fetch = FetchType.LAZY)
     @JsonIgnore
     private User user;
+
     public Integer getId() {
         return id;
     }
+
     public void setId(Integer id) {
         this.id = id;
     }
+
     public String getDescription() {
         return description;
     }
+
     public void setDescription(String description) {
         this.description = description;
     }
